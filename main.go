8d6b400bb51e2186db64daa88aee60b21a04d404
Daniel Xu <danielgg.coding@gmail.com>
2020-03-28 15:29:25 +0800

    MODIFY: listen on port 8081

--- main.go
+++ main.go
@@ -42,5 +42,5 @@ func main() {
 	router.GET("/cuisine/:cuisineId", controllers.GetCuisine(db))
 	router.GET("/cuisines", controllers.GetAllCuisine(db))
 	router.GET("/vote", controllers.VoteCuisine(db))
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run(":8081") // listen and serve on 0.0.0.0:8081
 }
